87d27d67e64c4925ffbb167054d0b60c04604b23
rosafish <zhour@carleton.edu>
2020-08-29 03:36:04 -0700

    add shell script for esnli run_glue params

---
+++ examples/text-classification/run_esnli.sh
@@ -0,0 +1,11 @@
+python run_glue.py \
+	--model_name_or_path bert-base-cased \
+	--task_name ESNLI \
+	--do_train \
+	--do_eval \
+	--data_dir /data/rosa/data/esnli \
+	--max_seq_length 128 \
+	--per_device_train_batch_size 32 \
+	--learning_rate 2e-5 \
+	--num_train_epochs 3.0 \
+	--output_dir /data/rosa/HF-transformers-20/examples/text-classification/esnli_outputs/
