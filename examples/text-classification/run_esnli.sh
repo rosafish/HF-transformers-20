b5f5b614943648f792a74370d43373cd332982ea
rosafish <zhour@carleton.edu>
2020-08-30 18:29:36 -0700

    overwrite output dir

--- examples/text-classification/run_esnli.sh
+++ examples/text-classification/run_esnli.sh
@@ -8,4 +8,5 @@ python run_glue.py \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
 	--num_train_epochs 3.0 \
-	--output_dir /data/rosa/HF-transformers-20/examples/text-classification/esnli_outputs/
+	--output_dir /data/rosa/HF-transformers-20/examples/text-classification/esnli_outputs/ \
+	--overwrite_output_dir
