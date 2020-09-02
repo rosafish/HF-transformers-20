bc32a85945d86226082892ddf7b333032f4e7205
rosafish <zhour@carleton.edu>
2020-09-02 00:41:01 -0700

    overwrite cache = true

--- examples/text-classification/run_esnli.sh
+++ examples/text-classification/run_esnli.sh
@@ -9,4 +9,5 @@ python run_glue.py \
 	--learning_rate 2e-5 \
 	--num_train_epochs 3.0 \
 	--output_dir /data/rosa/HF-transformers-20/examples/text-classification/esnli_outputs/ \
-	--overwrite_output_dir
+	--overwrite_output_dir \
+	--overwrite_cache 
