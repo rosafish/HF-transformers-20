d7652ae15cbb40182df955fe71dc783b57f649da
rosafish <zhour@carleton.edu>
2020-09-27 09:30:15 -0700

    rm the wrong data path in a shell script and moved another file

--- examples/text-classification/esnli/eval_esnli_expl_bertgen.sh
+++ examples/text-classification/esnli/eval_esnli_expl_bertgen.sh
@@ -2,7 +2,7 @@ python ../run_glue.py \
 	--model_name_or_path ./esnli_expl1_outputs_copy/ \
 	--task_name ESNLI \
 	--do_eval \
-	--data_dir /data/rosa/HF-transformers-20/examples/text-classification/hans/EDM_esnli_hans_expl/ \
+	--data_dir TODO \
 	--max_seq_length 128 \
 	--output_dir ./esnli_expl1_outputs_copy/ \
 	--overwrite_output_dir \
