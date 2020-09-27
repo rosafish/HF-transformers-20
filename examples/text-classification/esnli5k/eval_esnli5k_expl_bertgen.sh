5cb486da50ebcc74b41a8509e2d0ed87b21037ae
rosafish <zhour@carleton.edu>
2020-09-27 16:28:54 -0700

    eval esnli5k trained model on p+h+e gen expl

---
+++ examples/text-classification/esnli5k/eval_esnli5k_expl_bertgen.sh
@@ -0,0 +1,10 @@
+python run_glue.py \
+	--model_name_or_path ./esnli5k_ph_expl_outputs_copy/ \
+	--task_name ESNLI \
+	--do_eval \
+	--data_dir ./EDM_esnli5k_expl/ \
+	--max_seq_length 128 \
+	--output_dir ./esnli5k_ph_expl_outputs_copy/ \
+	--overwrite_output_dir \
+	--overwrite_cache \
+	--esnli_input_type p+h:a,expl1:b 
