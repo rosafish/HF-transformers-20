32456a7ab6c204f57646bdef0f52f39455b9467d
rosafish <zhour@carleton.edu>
2020-10-04 00:09:54 -0700

    training code for cose bert2bert

--- examples/EncoderDecoderModel/cos-e/cose_bert2bert_train.py
+++ examples/EncoderDecoderModel/cos-e/cose_bert2bert_train.py
@@ -12,6 +12,7 @@ from cose_processor import CoseInputFeatures, cose_examples_to_features
 from transformers import EncoderDecoderModel
 from transformers import Trainer, TrainingArguments
 
+
 def main():
     # set seeds
     torch.manual_seed(0)
@@ -22,8 +23,8 @@ def main():
     # paths and params
     train_qa_data_path = '/data/rosa/data/cos-e/data/v1.0/train_rand_split.jsonl'
     train_expl_data_path = '/data/rosa/data/cos-e/data/v1.0/cose_train_v1.0.jsonl'
-    cached_train_features_file = '../cache/cached_cose_tmp' 
-    save_trained_model_dir = "./cose_tmp_model/"
+    cached_train_features_file = '../cache/cached_train_cose' 
+    save_trained_model_dir = "./cose_trained_model/"
     max_seq_len = 128
 
     # Get examples
@@ -40,6 +41,38 @@ def main():
         train_features = cose_examples_to_features(train_examples, max_seq_len, tokenizer)
         logger.info("Saving training features into cached file %s", cached_train_features_file)
         torch.save(train_features, cached_train_features_file)
+
+    # Training
+    #initialize Bert2Bert
+    model = EncoderDecoderModel.from_encoder_decoder_pretrained('bert-base-uncased', 'bert-base-uncased') 
+
+    training_args = TrainingArguments(
+        output_dir='./checkpoint-train-results',    # output directory
+        per_device_train_batch_size=4,              # batch size per device during training
+        weight_decay=0.01,                          # strength of weight decay
+        logging_dir='./train-logs',                 # directory for storing logs
+        do_train=True,
+        num_train_epochs=10,                        # total number of training epochs
+        logging_steps=5000,
+        save_steps=5000,
+        overwrite_output_dir=True,
+        warmup_steps=1000,                          # number of warmup steps for learning rate scheduler
+    )
+
+    trainer = Trainer(
+        model=model,                         # the instantiated 🤗 Transformers model to be trained
+        args=training_args,                  # training arguments, defined above
+        train_dataset=train_features,        # training dataset
+        eval_dataset=train_features          # evaluation dataset, does not matter here b/c I don't eval during training
+    )
+
+    trainer.train()
+
+    # Save Model After Training
+    output_dir = save_trained_model_dir
+    cuda_id = "1" # since there's something running on 0
+
+    trainer.save_model(output_dir)
 
 
 if __name__=='__main__':
