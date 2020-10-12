23cae427c3cd1d1a774e386ef3bfb7e618bc0e74
rosafish <zhour@carleton.edu>
2020-10-12 14:15:23 -0700

    make path more succint

--- examples/text-classification/esnli/run_esnli.sh
+++ examples/text-classification/esnli/run_esnli.sh
@@ -8,7 +8,7 @@ python ../run_glue.py \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
 	--num_train_epochs 3.0 \
-	--output_dir /data/rosa/HF-transformers-20/examples/text-classification/esnli/esnli_phe_1empty_2bert_outputs/ \
+	--output_dir ./esnli_phe_1empty_2bert_outputs/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b
