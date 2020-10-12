7602b5a491426e42f6bafc3bce8b9703a82f494c
rosafish <zhour@carleton.edu>
2020-10-12 07:43:35 -0700

    error to csv for esnli expl1

--- examples/text-classification/esnli/eval_esnli_expl_bertgen.sh
+++ examples/text-classification/esnli/eval_esnli_expl_bertgen.sh
@@ -1,11 +1,11 @@
 python ../run_glue.py \
-	--model_name_or_path ./esnli_phe_1empty_outputs_copy/ \
+	--model_name_or_path ./esnli_expl1_outputs_copy/ \
 	--task_name ESNLI \
 	--do_eval \
-	--data_dir ./esnli_train_on_gen_expl/eval_dev_emptyexpl/ \
+	--data_dir /data/rosa/data/esnli/ \
 	--max_seq_length 128 \
-	--output_dir ./esnli_phe_1empty_outputs_copy/ \
+	--output_dir ./esnli_expl1_outputs_copy/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
-	--esnli_input_type p+h:a,expl1:b \
+	--esnli_input_type expl1:a \
 	--output_error_file 
