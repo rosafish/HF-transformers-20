8fb5463280af46b9aff4c3324ca84b57c4d95928
rosafish <zhour@carleton.edu>
2020-10-13 12:40:18 -0700

    t+e on gold expl phe using pretrained phe empty expl

--- examples/text-classification/esnli/run_esnli.sh
+++ examples/text-classification/esnli/run_esnli.sh
@@ -3,12 +3,12 @@ python ../run_glue.py \
 	--task_name ESNLI \
 	--do_train \
 	--do_eval \
-	--data_dir ./EDM_esnli_expl/ \
+	--data_dir /data/rosa/data/esnli/ \
 	--max_seq_length 128 \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
 	--num_train_epochs 3.0 \
-	--output_dir ./esnli_phe_1empty_2bert_outputs/ \
+	--output_dir ./esnli_phe_1empty_2gold_outputs/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b
