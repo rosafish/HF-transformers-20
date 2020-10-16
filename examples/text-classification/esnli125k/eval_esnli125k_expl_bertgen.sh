5ee8db0230809974cd8303afc072cc95ab862144
rosafish <zhour@carleton.edu>
2020-10-16 06:02:18 -0700

    eval esnli125k on bert expl (trained 1empty2gold)

---
+++ examples/text-classification/esnli125k/eval_esnli125k_expl_bertgen.sh
@@ -0,0 +1,10 @@
+python ../run_glue.py \
+	--model_name_or_path ./esnli125k_phe_1empty_2gold_outputs_copy/ \
+	--task_name ESNLI \
+	--do_eval \
+	--data_dir ./EDM_esnli125k_expl/ \
+	--max_seq_length 128 \
+	--output_dir ./esnli125k_phe_1empty_2gold_outputs_copy/ \
+	--overwrite_output_dir \
+	--overwrite_cache \
+	--esnli_input_type p+h:a,expl1:b 
