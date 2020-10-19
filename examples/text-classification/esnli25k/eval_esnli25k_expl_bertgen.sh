e7797f4a15a874d216f3ce2e4cf05013ccccb24e
rosafish <zhour@carleton.edu>
2020-10-18 23:52:06 -0700

    E-snli_25k ETP train using gold and evaluate using bert generated using esnli_full

--- examples/text-classification/esnli25k/eval_esnli25k_expl_bertgen.sh
+++ examples/text-classification/esnli25k/eval_esnli25k_expl_bertgen.sh
@@ -2,7 +2,7 @@ python ../run_glue.py \
 	--model_name_or_path ./esnli25k_phe_1empty_2gold_outputs_copy/ \
 	--task_name ESNLI \
 	--do_eval \
-	--data_dir ./EDM_esnli25k_expl/ \
+	--data_dir /data/rosa/HF-transformers-20/examples/text-classification/esnli/EDM_esnli_expl/ \
 	--max_seq_length 128 \
 	--output_dir ./esnli25k_phe_1empty_2gold_outputs_copy/ \
 	--overwrite_output_dir \
