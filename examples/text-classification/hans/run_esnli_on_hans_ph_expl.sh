4204bdb5798dadde30adfe4a168f2f9075f4fb0f
rosafish <zhour@carleton.edu>
2020-10-23 02:26:51 -0700

    do it for high quality expl

--- examples/text-classification/hans/run_esnli_on_hans_ph_expl.sh
+++ examples/text-classification/hans/run_esnli_on_hans_ph_expl.sh
@@ -3,11 +3,11 @@ python ../run_glue.py \
 	--task_name HANS \
 	--do_train \
     --do_eval \
-	--data_dir /data/rosa/data/hans/with_rosa_expl_1k_in_esnli_format/lex_prep_templates/low_quality_expl/ \
+	--data_dir /data/rosa/data/hans/with_rosa_expl_1k_in_esnli_format/lex_prep_templates/high_quality_expl/ \
 	--max_seq_length 128 \
     --per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
-	--output_dir ./esnli_phe_1empty_2hans1k_lowqexpl_1ksteps_outputs/ \
+	--output_dir ./esnli_phe_1empty_2hans1k_highqexpl_1ksteps_outputs/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b \
