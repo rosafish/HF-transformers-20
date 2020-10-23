d0c1ed0d07612f22c9cec45d5e99039962021758
rosafish <zhour@carleton.edu>
2020-10-23 02:42:25 -0700

    eval phemptyexpl on hans

--- examples/text-classification/hans/eval_esnli_on_hans_ph_expl.sh
+++ examples/text-classification/hans/eval_esnli_on_hans_ph_expl.sh
@@ -1,10 +1,10 @@
 python ../run_glue.py \
-	--model_name_or_path ../esnli/esnli_ph_expl_outputs_copy/ \
+	--model_name_or_path ../esnli/esnli_phe_1empty_outputs_copy/ \
 	--task_name HANS \
 	--do_eval \
-	--data_dir ./EDM_esnli_hans_expl/ \
+	--data_dir /data/rosa/data/hans/with_rosa_expl_1k_in_esnli_format/lex_prep_templates/high_quality_expl/ \
 	--max_seq_length 128 \
-	--output_dir ../esnli/esnli_ph_expl_outputs_copy/ \
+	--output_dir ../esnli/esnli_phe_1empty_outputs_copy/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b 
