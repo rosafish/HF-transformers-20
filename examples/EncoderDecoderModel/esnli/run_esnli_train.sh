b2f88ac3ba6d9d1aea6c7a47f3150f2018391426
rosafish <zhour@carleton.edu>
2020-11-01 21:49:09 -0800

    run 2k steps on 1k low q expl hans

--- examples/EncoderDecoderModel/esnli/run_esnli_train.sh
+++ examples/EncoderDecoderModel/esnli/run_esnli_train.sh
@@ -1,5 +1,5 @@
 python ./esnli_bert2bert_train.py\
-    -data_dir /data/rosa/data/hans/in_esnli_format/template_expls/train_1k/high_q_expl/ \
-    -cached_train_features_file ../cache/cached_train_hans1k_1temp_highqexpl \
-    -save_trained_model_dir ./save_best_models/ft_esnli_on_hans1k_1temp_highqexpl/ \
+    -data_dir /data/rosa/data/hans/in_esnli_format/template_expls/train_1k/low_q_expl/ \
+    -cached_train_features_file ../cache/cached_train_hans1k_1temp_lowqexpl \
+    -save_trained_model_dir ./save_best_models/ft_esnli_on_hans1k_1temp_lowqexpl/ \
     -max_steps 2000 
