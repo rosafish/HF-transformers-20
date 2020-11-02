cae905f163dc02cde760d6574b0ac2a31fbcbc3f
rosafish <zhour@carleton.edu>
2020-11-01 16:31:31 -0800

    add args and shell script for bert2bert training

---
+++ examples/EncoderDecoderModel/esnli/run_esnli_train.sh
@@ -0,0 +1,6 @@
+python ./esnli_bert2bert_train.py\
+    -data_dir /data/rosa/data/hans/in_esnli_format/template_expls/train_1k/high_q_expl/ \
+    -cached_train_features_file ../cache/cached_train_hans1k_1temp_highqexpl \
+    -save_trained_model_dir ./save_best_models/ft_esnli_on_hans1k_1temp_highqexpl/ \
+    -max_steps 1000 \
+    -hans 
