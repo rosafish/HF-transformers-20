ff95bc694a3296620a6f4d4520688b2f0b0c5253
rosafish <zhour@carleton.edu>
2020-11-04 21:27:22 -0800

    try on high qexpl

--- examples/EncoderDecoderModel/esnli/eval_esnli.sh
+++ examples/EncoderDecoderModel/esnli/eval_esnli.sh
@@ -1,3 +1,3 @@
 python ./esnli_bert2bert_eval.py\
     -model_dir ./save_best_models/esnli_train_trained_model/best_model/ \
-    -eval_data_path /data/rosa/data/hans/in_esnli_format/template_expls/train_25k/low_q_expl/esnli_dev.csv 
+    -eval_data_path /data/rosa/data/hans/in_esnli_format/template_expls/train_25k/high_q_expl/esnli_dev.csv 
