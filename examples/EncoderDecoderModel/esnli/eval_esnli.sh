49a70921c4ad09ee2f1be46928709375a78ca62f
rosafish <zhour@carleton.edu>
2020-11-04 20:32:45 -0800

    minor bug

--- examples/EncoderDecoderModel/esnli/eval_esnli.sh
+++ examples/EncoderDecoderModel/esnli/eval_esnli.sh
@@ -1,3 +1,3 @@
-python ./esnli_bert2bert_train.py\
-    -model_dir ./save_best_models/esnli_train_trained_model \
-    -eval_data_path /data/rosa/data/hans/in_esnli_format/template_expls/train_25k/low_q_expl/ 
+python ./esnli_bert2bert_eval.py\
+    -model_dir ./save_best_models/esnli_train_trained_model/ \
+    -eval_data_path /data/rosa/data/hans/in_esnli_format/template_expls/train_25k/low_q_expl/esnli_dev.csv 
