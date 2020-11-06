decb374b99d6d9a82d5d5335b07f605a797c2661
rosafish <zhour@carleton.edu>
2020-11-06 07:14:29 -0800

    ft esnli phe gold on hans 500 1T gold high q expl

--- examples/text-classification/esnli/run_esnli.sh
+++ examples/text-classification/esnli/run_esnli.sh
@@ -1,14 +1,14 @@
 python ../run_glue.py \
-	--model_name_or_path bert-base-cased \
+	--model_name_or_path ./save_best_model/esnli_ph_expl_outputs/best_model/ \
 	--task_name ESNLI \
 	--do_train \
 	--do_eval \
-	--data_dir ./esnli_train_dev_empty_expl/ \
+	--data_dir /data/rosa/data/hans/in_esnli_format/template_expls/train_500/high_q_expl/ \
 	--max_seq_length 128 \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
-	--num_train_epochs 5.0 \
-	--output_dir ./save_best_model/esnli_phe_emptyexpl_outputs/ \
+	--num_train_epochs 40.0 \
+	--output_dir ./save_best_model/esnli_phe_goldexpl_ft_HANS1T500_highqexpl_outputs/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b \
