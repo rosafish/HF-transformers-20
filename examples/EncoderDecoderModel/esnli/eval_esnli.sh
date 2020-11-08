30ce6ce2af896d5917e3779f49220d96a7c1ea03
rosafish <zhour@carleton.edu>
2020-11-08 09:42:42 -0800

    use hans ft model to evaluate on the original e-snli dev dataset again

--- examples/EncoderDecoderModel/esnli/eval_esnli.sh
+++ examples/EncoderDecoderModel/esnli/eval_esnli.sh
@@ -1,3 +1,3 @@
 python ./esnli_bert2bert_eval.py\
-    -model_dir ./save_best_models/esnli_train_trained_model/best_model/ \
-    -eval_data_path /data/rosa/data/hans/in_esnli_format/template_expls/train_25k/high_q_expl/esnli_dev.csv 
+    -model_dir ./save_best_models/ft_esnli_on_hans500_1temp_highqexpl/best_model/ \
+    -eval_data_path /data/rosa/data/esnli/snli_dev.csv 
