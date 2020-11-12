f9cd5516c068473db6c23b74812409e26bddf789
rosafish <zhour@carleton.edu>
2020-11-12 13:53:48 -0800

    phe empty expl benchmark hans 30t 300

--- examples/text-classification/esnli/run_esnli.sh
+++ examples/text-classification/esnli/run_esnli.sh
@@ -3,12 +3,12 @@ python ../run_glue.py \
 	--task_name HANS \
 	--do_train \
 	--do_eval \
-	--data_dir /data/rosa/data/hans/in_esnli_format/template_expls/train_500/high_q_expl/ \
+	--data_dir /data/rosa/data/hans/in_esnli_format/template_expls/30T_300/empty_q_expl/ \
 	--max_seq_length 128 \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
 	--num_train_epochs 40.0 \
-	--output_dir ./save_best_model/esnli_phe_goldexpl_ft_HANS1T500_highqexpl_outputs/ \
+	--output_dir ./save_best_model/esnli_phe_emptyexpl_ft_HANS30T300_outputs/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b \
