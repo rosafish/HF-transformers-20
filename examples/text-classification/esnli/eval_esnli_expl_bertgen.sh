8c00e19a3ee80896862517c14204544e024701d4
rosafish <zhour@carleton.edu>
2020-11-13 08:59:19 -0800

    eval hans phempty on snli

--- examples/text-classification/esnli/eval_esnli_expl_bertgen.sh
+++ examples/text-classification/esnli/eval_esnli_expl_bertgen.sh
@@ -1,10 +1,10 @@
 python ../run_glue.py \
-	--model_name_or_path ./esnli_phe_1empty_2gold_outputs_copy/ \
+	--model_name_or_path ./save_best_model/esnli_phe_emptyexpl_ft_HANS30T300_outputs/ \
 	--task_name ESNLI \
 	--do_eval \
-	--data_dir ./EDM_esnli_expl/ \
+	--data_dir ./esnli_train_dev_empty_expl/ \
 	--max_seq_length 128 \
-	--output_dir ./esnli_phe_1empty_2gold_outputs_copy/ \
+	--output_dir ./tmp/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b \
