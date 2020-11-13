6fb6e20b196a6ce15a212b9bfc3216867c7ca5ad
rosafish <zhour@carleton.edu>
2020-11-13 08:21:50 -0800

    phe empty expl hans 30T 3000

--- examples/text-classification/esnli/run_esnli.sh
+++ examples/text-classification/esnli/run_esnli.sh
@@ -1,15 +1,16 @@
 python ../run_glue.py \
-	--model_name_or_path ./save_best_model/esnli_ph_expl_outputs/best_model/ \
-	--task_name HANS \
+	--model_name_or_path ./save_best_model/esnli_phe_emptyexpl_outputs/best_model/ \
+	--task_name ESNLI \
 	--do_train \
 	--do_eval \
-	--data_dir /data/rosa/data/hans/in_esnli_format/template_expls/30T_300/empty_q_expl/ \
+	--data_dir /data/rosa/data/hans/in_esnli_format/template_expls/30T_3000/shuffled/empty_expl/ \
 	--max_seq_length 128 \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
-	--num_train_epochs 40.0 \
-	--output_dir ./save_best_model/esnli_phe_emptyexpl_ft_HANS30T300_outputs/ \
+	--num_train_epochs 10.0 \
+	--output_dir ./save_best_model/esnli_phe_emptyexpl_ft_HANS30T3000_outputs/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b \
-	--save_best_model
+	--save_best_model \
+	--logging_steps 50
