1dce1dfb5dd9abfffc4f0018e58a7afc24388e41
rosafish <zhour@carleton.edu>
2020-11-17 11:50:45 -0800

    ph empty expl hans 30T 300 with unseen

--- examples/text-classification/esnli/run_esnli.sh
+++ examples/text-classification/esnli/run_esnli.sh
@@ -3,12 +3,12 @@ python ../run_glue.py \
 	--task_name ESNLI \
 	--do_train \
 	--do_eval \
-	--data_dir /data/rosa/data/hans/in_esnli_format/template_expls/30T_300/empty_expl/ \
+	--data_dir /data/rosa/data/hans/in_esnli_format/template_expls/30T/with_unseen/300/empty_expl/ \
 	--max_seq_length 128 \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
 	--num_train_epochs 30.0 \
-	--output_dir ./save_best_model/esnli_phe_emptyexpl_ft_HANS30T300_outputs/ \
+	--output_dir ./save_best_model/esnli_phe_emptyexpl_ft_HANS30T300_unseen_outputs/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b \
