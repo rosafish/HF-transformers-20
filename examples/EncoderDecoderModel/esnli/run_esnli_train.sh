6a3e55efcbb6cbe475f0a3c0ba93d5e9560f4395
rosafish <zhour@carleton.edu>
2020-11-19 14:37:16 -0800

    ft_bert_on_hans30T_300train_unseen_lowqexpl

--- examples/EncoderDecoderModel/esnli/run_esnli_train.sh
+++ examples/EncoderDecoderModel/esnli/run_esnli_train.sh
@@ -1,5 +1,5 @@
 python ./esnli_bert2bert_train.py\
-    -data_dir /data/rosa/data/hans/in_esnli_format/template_expls/30T/with_unseen/1500/high_q_expl/ \
-    -cached_train_features_file ../cache/cached_train_hans30T_1500train_unseen_highqexpl \
-    -save_trained_model_dir ./save_best_models/ft_bert_on_hans30T_1500train_unseen_highqexpl/ \
-    -max_steps 4000 
+    -data_dir /data/rosa/data/hans/in_esnli_format/template_expls/30T/with_unseen/300/low_q_expl/ \
+    -cached_train_features_file ../cache/cached_train_hans30T_300train_unseen_lowqexpl \
+    -save_trained_model_dir ./save_best_models/ft_bert_on_hans30T_300train_unseen_lowqexpl/ \
+    -max_steps 2000 
