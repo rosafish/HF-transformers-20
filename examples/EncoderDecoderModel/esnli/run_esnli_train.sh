dff8c73a4dc38d78aaa05b7f8a2ff4df2a46c00c
rosafish <zhour@carleton.edu>
2020-11-20 05:53:41 -0800

    10000 steps for ft_bert_on_hans30T_2400train_unseen_lowqexpl

--- examples/EncoderDecoderModel/esnli/run_esnli_train.sh
+++ examples/EncoderDecoderModel/esnli/run_esnli_train.sh
@@ -2,6 +2,6 @@ python ./esnli_bert2bert_train.py\
     -data_dir /data/rosa/data/hans/in_esnli_format/template_expls/30T/with_unseen/24_6_split/2400train/low_q_expl/ \
     -cached_train_features_file ../cache/cached_train_hans30T_2400train_unseen_lowqexpl \
     -save_trained_model_dir ./save_best_models/ft_bert_on_hans30T_2400train_unseen_lowqexpl/ \
-    -max_steps 4000 \
+    -max_steps 10000 \
     -eval_method step \
-    -eval_steps 500 
+    -eval_steps 1000 
