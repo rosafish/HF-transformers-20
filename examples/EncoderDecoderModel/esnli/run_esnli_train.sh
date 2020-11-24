68925058f551e7839ebe7bfdbd1d2a51cf46fd95
rosafish <zhour@carleton.edu>
2020-11-23 22:56:33 -0800

    debug_12000_highqexpl

--- examples/EncoderDecoderModel/esnli/run_esnli_train.sh
+++ examples/EncoderDecoderModel/esnli/run_esnli_train.sh
@@ -1,8 +1,8 @@
 python ./esnli_bert2bert_train.py\
     -model_dir ./save_best_models/esnli_train_trained_model/best_model/ \
-    -data_dir /data/rosa/data/hans/in_esnli_format/template_expls/30T/with_unseen/24_6_split/12000train/low_q_expl/ \
-    -cached_train_features_file ../cache/debug_12000_lowqexpl \
-    -save_trained_model_dir ./save_best_models/debug_12000_lowqexpl \
+    -data_dir /data/rosa/data/hans/in_esnli_format/template_expls/30T/with_unseen/24_6_split/12000train/high_q_expl/ \
+    -cached_train_features_file ../cache/debug_12000_highqexpl \
+    -save_trained_model_dir ./save_best_models/debug_12000_highqexpl \
     -max_steps 10000 \
     -eval_method step \
     -eval_steps 1000 
