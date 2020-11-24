2a8d51f9edb04ebbebfa827ff80bf94c56f2b84b
rosafish <zhour@carleton.edu>
2020-11-24 12:20:03 -0800

    eval esnli ft on hans model back on esnli

--- examples/EncoderDecoderModel/esnli/eval_esnli.sh
+++ examples/EncoderDecoderModel/esnli/eval_esnli.sh
@@ -1,3 +1,3 @@
 python ./esnli_bert2bert_eval.py\
-    -model_dir ./save_best_models/ft_esnli_on_hans500_1temp_highqexpl/best_model/ \
-    -eval_data_path /data/rosa/data/esnli/snli_dev.csv 
+    -model_dir ./save_best_models/ft_esnli_on_hans30T_300train_unseen_highqexpl/best_model/ \
+    -eval_data_path /data/rosa/data/esnli/esnli_dev.csv 
