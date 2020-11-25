40dbbb88818bf3a9b862b0ce4de031965594bcc9
rosafish <zhour@carleton.edu>
2020-11-24 22:30:07 -0800

    mnli 0shot on mismatched dev

--- examples/EncoderDecoderModel/esnli/eval_esnli.sh
+++ examples/EncoderDecoderModel/esnli/eval_esnli.sh
@@ -1,3 +1,3 @@
 python ./esnli_bert2bert_eval.py\
     -model_dir ./save_best_models/esnli_train_trained_model/best_model/ \
-    -eval_data_path /data/rosa/data/glue_mnli_esnli_format/matched_dev/esnli_dev.csv
+    -eval_data_path /data/rosa/data/glue_mnli_esnli_format/mismatched_dev/esnli_dev.csv
