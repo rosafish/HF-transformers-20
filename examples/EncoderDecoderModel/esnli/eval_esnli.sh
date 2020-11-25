b1b10a3fbc69263ed703081ceda341625ad65915
rosafish <zhour@carleton.edu>
2020-11-25 14:00:08 -0800

    mnli 0shot gen expl training data

--- examples/EncoderDecoderModel/esnli/eval_esnli.sh
+++ examples/EncoderDecoderModel/esnli/eval_esnli.sh
@@ -1,3 +1,4 @@
 python ./esnli_bert2bert_eval.py\
     -model_dir ./save_best_models/esnli_train_trained_model/best_model/ \
-    -eval_data_path /data/rosa/data/glue_mnli_esnli_format/matched_dev/esnli_train.csv
+    -eval_data_path /data/rosa/data/glue_mnli_esnli_format/matched_dev/esnli_train.csv \
+    -generate_expl_on_training_data
