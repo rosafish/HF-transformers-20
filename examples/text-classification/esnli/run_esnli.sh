e59d32fd50272715d82e982b41bcb1ea990e2b74
rosafish <zhour@carleton.edu>
2020-11-25 15:08:48 -0800

    draft for running for HANS ETP

--- examples/text-classification/esnli/run_esnli.sh
+++ examples/text-classification/esnli/run_esnli.sh
@@ -1,14 +1,14 @@
 python ../run_glue.py \
-	--model_name_or_path ./save_best_model/esnli_phe_emptyexpl_outputs/best_model/ \
+	--model_name_or_path ./save_best_model/TODO/best_model/ \
 	--task_name ESNLI \
 	--do_train \
 	--do_eval \
-	--data_dir /data/rosa/data/hans/in_esnli_format/template_expls/30T/with_unseen/1500/empty_expl/ \
+	--data_dir /data/rosa/HF-transformers-20/examples/text-classification/esnli/data/HANS_ETP_30T_unseen_300train_highqexpl/ \
 	--max_seq_length 128 \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
-	--num_train_epochs 20.0 \
-	--output_dir ./save_best_model/esnli_phe_emptyexpl_ft_HANS30T1500_unseen_outputs/ \
+	--num_train_epochs 40.0 \
+	--output_dir ./save_best_model/esnli_ETP_ft_HANS30T300_unseen_highqexpl_outputs/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b \
