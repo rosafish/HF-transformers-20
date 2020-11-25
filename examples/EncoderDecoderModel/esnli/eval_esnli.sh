8f4277ce1afe1b20d22ec90f6ef0b1961950279d
rosafish <zhour@carleton.edu>
2020-11-24 21:56:27 -0800

    MNLI 0-shot learning generate expl embeddings for matched dev

--- examples/EncoderDecoderModel/esnli/eval_esnli.sh
+++ examples/EncoderDecoderModel/esnli/eval_esnli.sh
@@ -1,3 +1,3 @@
 python ./esnli_bert2bert_eval.py\
-    -model_dir ./save_best_models/ft_esnli_on_hans30T_12000train_unseen_lowqexpl/best_model/ \
-    -eval_data_path /data/rosa/data/esnli/esnli_dev.csv 
+    -model_dir ./save_best_models/esnli_train_trained_model/best_model/ \
+    -eval_data_path /data/rosa/data/glue_mnli_esnli_format/matched_dev/esnli_dev.csv
