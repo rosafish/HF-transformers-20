3da08233e61290331877e5c173f5e1b28942f172
rosafish <zhour@carleton.edu>
2020-11-27 02:13:26 -0800

    esnli_ETP_ft_HANS30T1200_unseen_highqexpl_outputs

--- examples/text-classification/esnli/run_esnli.sh
+++ examples/text-classification/esnli/run_esnli.sh
@@ -3,15 +3,15 @@ python ../run_glue.py \
 	--task_name ESNLI \
 	--do_train \
 	--do_eval \
-	--data_dir /data/rosa/HF-transformers-20/examples/text-classification/esnli/data/HANS_ETP_30T_unseen_240train_highqexpl/ \
+	--data_dir /data/rosa/HF-transformers-20/examples/text-classification/esnli/data/HANS_ETP_30T_unseen_1200train_highqexpl/ \
 	--max_seq_length 128 \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
-	--output_dir ./save_best_model/esnli_ETP_ft_HANS30T240_unseen_highqexpl_outputs/ \
+	--output_dir ./save_best_model/esnli_ETP_ft_HANS30T1200_unseen_highqexpl_outputs/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b \
 	--save_best_model \
-	--eval_method epoch \
-	--max_steps 400 \
-	--eval_steps 100
+	--eval_method step \
+	--max_steps 100 \
+	--eval_steps 4
