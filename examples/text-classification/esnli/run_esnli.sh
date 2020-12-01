bbe070d3bddbb3416b4a1f3e03b40b6e8bd5d8bf
rosafish <zhour@carleton.edu>
2020-12-01 14:05:18 -0800

    esnli_mnli0shot_phe_emptyexpl_matched_dev_outputs

--- examples/text-classification/esnli/run_esnli.sh
+++ examples/text-classification/esnli/run_esnli.sh
@@ -1,5 +1,5 @@
 python ../run_glue.py \
-	--model_name_or_path bert-base-cased \
+	--model_name_or_path ./save_best_model/esnli_phe_emptyexpl_outputs/ \
 	--task_name ESNLI \
 	--do_train \
 	--do_eval \
@@ -7,7 +7,7 @@ python ../run_glue.py \
 	--max_seq_length 128 \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
-	--output_dir ./save_best_model/bert_mnli0shot_phe_emptyexpl_matched_dev_outputs/ \
+	--output_dir ./save_best_model/esnli_mnli0shot_phe_emptyexpl_matched_dev_outputs/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b \
