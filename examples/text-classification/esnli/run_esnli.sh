c9f51dcf7195b74269c926e57ac2bd21d7053537
rosafish <zhour@carleton.edu>
2020-12-01 14:09:53 -0800

    esnli_mnli0shot_phe_emptyexpl_matched_dev_outputs

--- examples/text-classification/esnli/run_esnli.sh
+++ examples/text-classification/esnli/run_esnli.sh
@@ -1,5 +1,5 @@
 python ../run_glue.py \
-	--model_name_or_path ./save_best_model/esnli_phe_emptyexpl_outputs/ \
+	--model_name_or_path ./save_best_model/esnli_phe_emptyexpl_outputs/best_model/ \
 	--task_name ESNLI \
 	--do_train \
 	--do_eval \
