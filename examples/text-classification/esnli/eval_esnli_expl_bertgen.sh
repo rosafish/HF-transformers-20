030d7c9dcd33cfd8ca5c0da49a8c0e3b0df4797e
rosafish <zhour@carleton.edu>
2020-12-02 15:08:57 -0800

    0-shot eval on mnli matched dev

--- examples/text-classification/esnli/eval_esnli_expl_bertgen.sh
+++ examples/text-classification/esnli/eval_esnli_expl_bertgen.sh
@@ -1,8 +1,8 @@
 python ../run_glue.py \
-	--model_name_or_path ./save_best_model/esnli_phe_emptyexpl_ft_HANS30T300_outputs/best_model/ \
+	--model_name_or_path ./save_best_model/esnli_phe_goldexpl_outputs/best_model/ \
 	--task_name ESNLI \
 	--do_eval \
-	--data_dir ./esnli_train_dev_empty_expl/ \
+	--data_dir ./data/eval_mnli_ETP_matched_dev/ \
 	--max_seq_length 128 \
 	--output_dir ./tmp/ \
 	--overwrite_output_dir \
