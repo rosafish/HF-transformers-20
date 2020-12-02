abb73f5d8df912750fbfb86750638de2d4c9c752
rosafish <zhour@carleton.edu>
2020-12-02 15:13:55 -0800

    eval eval_mnli_ETP_mismatched_dev

--- examples/text-classification/esnli/eval_esnli_expl_bertgen.sh
+++ examples/text-classification/esnli/eval_esnli_expl_bertgen.sh
@@ -2,7 +2,7 @@ python ../run_glue.py \
 	--model_name_or_path ./save_best_model/esnli_phe_goldexpl_outputs/best_model/ \
 	--task_name ESNLI \
 	--do_eval \
-	--data_dir ./data/eval_mnli_ETP_matched_dev/ \
+	--data_dir ./data/eval_mnli_ETP_mismatched_dev/ \
 	--max_seq_length 128 \
 	--output_dir ./tmp/ \
 	--overwrite_output_dir \
