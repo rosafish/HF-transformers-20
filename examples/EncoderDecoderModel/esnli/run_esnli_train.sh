0090e98e27cba35b1b270a9cffa024f69e916d97
rosafish <zhour@carleton.edu>
2021-01-05 10:44:53 -0800

    bert_hans_seed0_train240_low

--- examples/EncoderDecoderModel/esnli/run_esnli_train.sh
+++ examples/EncoderDecoderModel/esnli/run_esnli_train.sh
@@ -1,9 +1,9 @@
 python ./esnli_bert2bert_train.py\
     -model_dir bert-base-uncased \
-    -train_data_path /data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/seed0/train240_high.csv \
-    -eval_data_path /data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/seed0/dev2400_high.csv \
-    -cached_train_features_file ../cache/bert_hans_seed0_train240_high \
-    -save_trained_model_dir ./save_best_models/bert_hans_seed0_train240_high/ \
+    -train_data_path /data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/seed0/train240_low.csv \
+    -eval_data_path /data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/seed0/dev2400_low.csv \
+    -cached_train_features_file ../cache/bert_hans_seed0_train240_low \
+    -save_trained_model_dir ./save_best_models/bert_hans_seed0_train240_low/ \
     -max_steps 4000 \
     -eval_method step \
     -eval_steps 1000 
