1c78e408abdd9d274788aa8a37b7a1dce1075bc5
rosafish <zhour@carleton.edu>
2021-01-05 00:52:11 -0800

    bert_hans_seed0_train240_ex_low

--- examples/EncoderDecoderModel/esnli/run_esnli_train.sh
+++ examples/EncoderDecoderModel/esnli/run_esnli_train.sh
@@ -3,7 +3,7 @@ python ./esnli_bert2bert_train.py\
     -train_data_path /data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/seed0/train240_ex_low.csv \
     -eval_data_path /data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/seed0/dev2400_ex_low.csv \
     -cached_train_features_file ../cache/bert_hans_seed0_train240_ex_low \
-    -save_trained_model_dir ./save_best_models/bert_hans_seed0_train240_ex_low \
-    -max_steps 2000 \
+    -save_trained_model_dir ./save_best_models/bert_hans_seed0_train240_ex_low/ \
+    -max_steps 4000 \
     -eval_method step \
-    -eval_steps 500 
+    -eval_steps 1000 
