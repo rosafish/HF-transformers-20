1028dba99b5c65bbbcf5504ccca173cae9413091
Yangqiaoyu Zhou <zhouy1@fe01.ai.cs.uchicago.edu>
2021-01-05 15:28:34 -0600

    bert_hans_seed0_train1200_low

--- examples/EncoderDecoderModel/esnli/run_esnli_train.sh
+++ examples/EncoderDecoderModel/esnli/run_esnli_train.sh
@@ -1,9 +1,9 @@
 python ./esnli_bert2bert_train.py\
     -model_dir bert-base-uncased \
-    -train_data_path /data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/seed0/train240_low.csv \
-    -eval_data_path /data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/seed0/dev2400_low.csv \
-    -cached_train_features_file ../cache/bert_hans_seed0_train240_low \
-    -save_trained_model_dir ./save_best_models/bert_hans_seed0_train240_low/ \
+    -train_data_path ~/data/randomness_experiment/seed0/train1200_low.csv \
+    -eval_data_path ~/data/randomness_experiment/seed0/dev2400_low.csv \
+    -cached_train_features_file ../cache/bert_hans_seed0_train1200_low \
+    -save_trained_model_dir ./save_best_models/bert_hans_seed0_train1200_low/ \
     -max_steps 4000 \
     -eval_method step \
     -eval_steps 1000 
