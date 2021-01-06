9a1d1e8007389606134ce550559dc9f8fc5f640c
rosafish <zhour@carleton.edu>
2021-01-06 08:47:04 -0800

    eval on matched test bert_hans_seed0_train240_low

--- examples/text-classification/esnli/eval_esnli_expl_bertgen.sh
+++ examples/text-classification/esnli/eval_esnli_expl_bertgen.sh
@@ -1,11 +1,9 @@
 python ../run_glue.py \
-	--model_name_or_path bert-base-cased \
+	--model_name_or_path ./save_best_model/bert_hans_seed0_train240_low/best_model/ \
 	--task_name ESNLI \
 	--do_eval \
-	--data_dir ./data/eval_mnli_ETP_mismatched_dev/ \
+	--dev_data_path /data/rosa/HF-transformers-20/examples/EncoderDecoderModel/esnli/save_best_models/bert_hans_seed0_train240_low/matched_test_text_esnli_format.csv \
 	--max_seq_length 128 \
-	--output_dir ./tmp/ \
-	--overwrite_output_dir \
+	--output_dir ./save_best_model/bert_hans_seed0_train240_low/ \
 	--overwrite_cache \
-	--esnli_input_type p+h:a,expl1:b \
-	--output_error_file 
+	--esnli_input_type p+h:a,expl1:b 
