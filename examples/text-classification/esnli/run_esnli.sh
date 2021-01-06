9981f4d06413cde96f94178162e4de56a0bbc0fb
rosafish <zhour@carleton.edu>
2021-01-06 15:31:34 -0800

    add var to sh file

--- examples/text-classification/esnli/run_esnli.sh
+++ examples/text-classification/esnli/run_esnli.sh
@@ -1,15 +1,19 @@
+quality=high
+seed=0
+training_size=240
+
 python ../run_glue.py \
 	--model_name_or_path bert-base-uncased \
 	--task_name ESNLI \
 	--do_train \
 	--do_eval \
-	--train_data_path /data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/seed0/train240_low.csv \
-	--dev_data_path /data/rosa/HF-transformers-20/examples/EncoderDecoderModel/esnli/save_best_model/bert_hans_seed0_train240_low/dev_text_esnli_format.csv \
-	--test_data_path /data/rosa/HF-transformers-20/examples/EncoderDecoderModel/esnli/save_best_models/bert_hans_seed0_train240_low/matched_test_text_esnli_format.csv \
+	--train_data_path /data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/seed${seed}/train${training_size}_${quality}.csv \
+	--dev_data_path /data/rosa/HF-transformers-20/examples/EncoderDecoderModel/esnli/save_best_model/bert_hans_seed${seed}_train${training_size}_${quality}/dev_text_esnli_format.csv \
+	--test_data_path /data/rosa/HF-transformers-20/examples/EncoderDecoderModel/esnli/save_best_models/bert_hans_seed${seed}_train${training_size}_${quality}/matched_test_text_esnli_format.csv \
 	--max_seq_length 128 \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
-	--output_dir ./save_best_model/bert_hans_seed0_train240_low/ \
+	--output_dir ./save_best_model/bert_hans_seed${seed}_train${training_size}_${quality}/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b \
