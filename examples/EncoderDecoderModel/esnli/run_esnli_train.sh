804b4abc3a940c2b8234b461a572a64f75f0ec40
rosafish <rosazhou1997@gmail.com>
2021-01-05 23:27:06 -0600

    slurm bert_hans_seed0_train12000_high

--- examples/EncoderDecoderModel/esnli/run_esnli_train.sh
+++ examples/EncoderDecoderModel/esnli/run_esnli_train.sh
@@ -3,7 +3,7 @@ python ./esnli_bert2bert_train.py\
     -train_data_path ~/data/randomness_experiment/seed0/train12000_high.csv \
     -eval_data_path ~/data/randomness_experiment/seed0/dev2400_high.csv \
     -cached_train_features_file ../cache/bert_hans_seed0_train12000_high \
-    -save_trained_model_dir ./save_best_models/bert_hans_seed0_train12000_high/ \
+    -save_trained_model_dir /net/scratch/zhouy1/randomness_experiment/edm/bert_hans_seed0_train12000_high/ \
     -max_steps 10000 \
     -eval_method step \
     -eval_steps 2000 
