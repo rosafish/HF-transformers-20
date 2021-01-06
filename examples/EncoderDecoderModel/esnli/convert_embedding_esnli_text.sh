dc6455392518f918c0954d25fc6b8869aaf12e74
rosafish <zhour@carleton.edu>
2021-01-06 15:21:43 -0800

    seed0 240 high and ex_low convert to esnli text format

--- examples/EncoderDecoderModel/esnli/convert_embedding_esnli_text.sh
+++ examples/EncoderDecoderModel/esnli/convert_embedding_esnli_text.sh
@@ -1,12 +1,14 @@
-dir=./save_best_models/bert_hans_seed0_train240_low/
-data_type=matched_test
+# inputs
+data_type=dev
 input_file_name=epoch266*.csv
-quality=low
+quality=ex_low
+
+dir=./save_best_models/bert_hans_seed0_train240_${quality}/
 
 if [ $data_type = dev ]; then
     # dev
     python convert_generated_embedding_text.py \
-    -embedding_csv_path ${dir}input_file_name \
+    -embedding_csv_path ${dir}${input_file_name} \
     -text_csv_path ${dir}dev_text.csv 
 
     python convert_bertgen_to_original_format.py \
