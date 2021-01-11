aa0203497562f9b340a5e25edb25faf34e65bc48
rosafish <zhour@carleton.edu>
2021-01-11 09:57:18 -0800

    add sh var for pretrained model type

--- examples/EncoderDecoderModel/esnli/run_esnli_train.sh
+++ examples/EncoderDecoderModel/esnli/run_esnli_train.sh
@@ -1,5 +1,17 @@
+pretrained_model = esnli # esnli or bert
+
+if [ $pretrained_model = esnli ]; then
+
+    model_dir = ./save_best_models/esnli_train_trained_model/best_model/
+
+elif [ $pretrained_model = bert ]; then
+
+    model_dir = bert-base-uncased
+
+fi
+
 python ./esnli_bert2bert_train.py\
-    -model_dir bert-base-uncased \
+    -model_dir $model_dir \
     -train_data_path ~/data/randomness_experiment/seed1/train12000_high.csv \
     -eval_data_path ~/data/randomness_experiment/seed1/dev2400_high.csv \
     -cached_train_features_file ../cache/bert_hans_seed1_train12000_high \
