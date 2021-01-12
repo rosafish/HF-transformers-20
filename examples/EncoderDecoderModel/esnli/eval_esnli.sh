7a72668082557621532502c0241f75f4d65d54c5
rosafish <zhour@carleton.edu>
2021-01-12 15:35:18 -0800

    add sh vars for eval

--- examples/EncoderDecoderModel/esnli/eval_esnli.sh
+++ examples/EncoderDecoderModel/esnli/eval_esnli.sh
@@ -1,4 +1,9 @@
+quality=high
+test_type=mismatched
+seed=0
+train_size=240
+
 python ./esnli_bert2bert_eval.py\
-    -model_dir ./save_best_models/bert_hans_seed0_train240_ex_low/best_model/ \
-    -eval_data_path /data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/seed0/matched_test3000_ex_low.csv \
-    -eval_results_dir ./save_best_models/bert_hans_seed0_train240_ex_low/eval_matched_test/ 
+    -model_dir ./save_best_models/bert_hans_seed${seed}_train${train_size}_${quality}/best_model/ \
+    -eval_data_path /data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/seed${seed}/${test_type}_test3000_${quality}.csv \
+    -eval_results_dir ./save_best_models/bert_hans_seed${seed}_train${train_size}_${quality}/eval_${test_type}_test/ 
