aefacec3b93e6345ccfe7bb2b6aec8e12cd3c093
rosafish <zhour@carleton.edu>
2021-01-12 20:33:56 -0800

    add pretrained model sh var

--- examples/text-classification/esnli/eval_esnli_expl_bertgen.sh
+++ examples/text-classification/esnli/eval_esnli_expl_bertgen.sh
@@ -1,14 +1,15 @@
-quality=high
+quality=low
 seed=0
 training_size=240
 test_type=mismatched
+pretrained_model=esnli #bert or esnli
 
 python ../run_glue.py \
-	--model_name_or_path ./save_best_model/bert_hans_seed${seed}_train${training_size}_${quality}/best_model/ \
+	--model_name_or_path ./save_best_model/${pretrained_model}_hans_seed${seed}_train${training_size}_${quality}/best_model/ \
 	--task_name ESNLI \
 	--do_eval \
-	--dev_data_path /data/rosa/HF-transformers-20/examples/EncoderDecoderModel/esnli/save_best_models/bert_hans_seed${seed}_train${training_size}_${quality}/${test_type}_test_text_esnli_format.csv \
+	--dev_data_path /data/rosa/HF-transformers-20/examples/EncoderDecoderModel/esnli/save_best_models/${pretrained_model}_hans_seed${seed}_train${training_size}_${quality}/${test_type}_test_text_esnli_format.csv \
 	--max_seq_length 128 \
-	--output_dir ./save_best_model/bert_hans_seed${seed}_train${training_size}_${quality}/eval_${test_type}_test/ \
+	--output_dir ./save_best_model/${pretrained_model}_hans_seed${seed}_train${training_size}_${quality}/eval_${test_type}_test/ \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b 
--- examples/text-classification/esnli/run_esnli.sh
+++ examples/text-classification/esnli/run_esnli.sh
@@ -1,7 +1,8 @@
-quality=ex_low
+quality=low
 seed=0
 training_size=240
 test_type=matched
+pretrained_model=esnli #bert or esnli
 
 python ../run_glue.py \
 	--model_name_or_path bert-base-uncased \
@@ -9,12 +10,12 @@ python ../run_glue.py \
 	--do_train \
 	--do_eval \
 	--train_data_path /data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/seed${seed}/train${training_size}_${quality}.csv \
-	--dev_data_path /data/rosa/HF-transformers-20/examples/EncoderDecoderModel/esnli/save_best_models/bert_hans_seed${seed}_train${training_size}_${quality}/dev_text_esnli_format.csv \
-	--test_data_path /data/rosa/HF-transformers-20/examples/EncoderDecoderModel/esnli/save_best_models/bert_hans_seed${seed}_train${training_size}_${quality}/${test_type}_test_text_esnli_format.csv \
+	--dev_data_path /data/rosa/HF-transformers-20/examples/EncoderDecoderModel/esnli/save_best_models/${pretrained_model}_hans_seed${seed}_train${training_size}_${quality}/dev_text_esnli_format.csv \
+	--test_data_path /data/rosa/HF-transformers-20/examples/EncoderDecoderModel/esnli/save_best_models/${pretrained_model}_hans_seed${seed}_train${training_size}_${quality}/${test_type}_test_text_esnli_format.csv \
 	--max_seq_length 128 \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
-	--output_dir ./save_best_model/bert_hans_seed${seed}_train${training_size}_${quality}/ \
+	--output_dir ./save_best_model/${pretrained_model}_hans_seed${seed}_train${training_size}_${quality}/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b \
