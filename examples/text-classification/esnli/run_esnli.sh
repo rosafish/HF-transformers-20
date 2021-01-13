8c8dc9fbe6ad2ea33142bbd9f1b013570d2b1dff
rosafish <zhour@carleton.edu>
2021-01-12 19:24:18 -0800

    add test type sh var

--- examples/text-classification/esnli/run_esnli.sh
+++ examples/text-classification/esnli/run_esnli.sh
@@ -1,6 +1,7 @@
 quality=ex_low
 seed=0
 training_size=240
+test_type=matched
 
 python ../run_glue.py \
 	--model_name_or_path bert-base-uncased \
@@ -9,7 +10,7 @@ python ../run_glue.py \
 	--do_eval \
 	--train_data_path /data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/seed${seed}/train${training_size}_${quality}.csv \
 	--dev_data_path /data/rosa/HF-transformers-20/examples/EncoderDecoderModel/esnli/save_best_models/bert_hans_seed${seed}_train${training_size}_${quality}/dev_text_esnli_format.csv \
-	--test_data_path /data/rosa/HF-transformers-20/examples/EncoderDecoderModel/esnli/save_best_models/bert_hans_seed${seed}_train${training_size}_${quality}/matched_test_text_esnli_format.csv \
+	--test_data_path /data/rosa/HF-transformers-20/examples/EncoderDecoderModel/esnli/save_best_models/bert_hans_seed${seed}_train${training_size}_${quality}/${test_type}_test_text_esnli_format.csv \
 	--max_seq_length 128 \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
