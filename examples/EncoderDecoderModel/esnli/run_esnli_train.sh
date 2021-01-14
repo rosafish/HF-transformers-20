f212aad48732be8336ae0d6e94fc18800975f9ff
rosafish <zhour@carleton.edu>
2021-01-14 00:37:58 -0800

    different eval method according to train size, different dev size according to train size

--- examples/EncoderDecoderModel/esnli/run_esnli_train.sh
+++ examples/EncoderDecoderModel/esnli/run_esnli_train.sh
@@ -1,6 +1,7 @@
 pretrained_model=bert # esnli or bert
 seed=0
 train_size=240
+dev_size=train_size*0.2
 quality=ex_low
 server=uchi # ego or uchi
 debug=false
@@ -27,30 +28,42 @@ elif [ $server = uchi ]; then
 
 fi
 
-if [ $train_size -lt 2000 ]; then
-
-    max_steps=4000
-    eval_steps=1000
-
-else
-
-    max_steps=10000
-    eval_steps=2000
-
-fi
-
 if [ $debug = true ]; then
 
     pretrained_model=debug
 
 fi
 
-python ./esnli_bert2bert_train.py\
+if [ $train_size -lt 2000 ]; then
+
+    max_steps=2000
+    eval_steps=200
+    eval_method=step
+
+    python ./esnli_bert2bert_train.py\
     -model_dir $model_dir \
     -train_data_path ${data_path_prefix}seed${seed}/train${train_size}_${quality}.csv \
-    -eval_data_path ${data_path_prefix}seed${seed}/dev2400_${quality}.csv \
+    -eval_data_path ${data_path_prefix}seed${seed}/dev${dev_size}_${quality}.csv \
     -cached_train_features_file ../cache/${pretrained_model}_hans_seed${seed}_train${train_size}_${quality} \
     -save_trained_model_dir ${save_model_path_prefix}${pretrained_model}_hans_seed${seed}_train${train_size}_${quality}/ \
     -max_steps $max_steps \
-    -eval_method step \
+    -eval_method $eval_method \
     -eval_steps $eval_steps 
+
+else
+
+    train_epochs=10
+    eval_method=epoch
+
+    python ./esnli_bert2bert_train.py\
+    -model_dir $model_dir \
+    -train_data_path ${data_path_prefix}seed${seed}/train${train_size}_${quality}.csv \
+    -eval_data_path ${data_path_prefix}seed${seed}/dev${dev_size}_${quality}.csv \
+    -cached_train_features_file ../cache/${pretrained_model}_hans_seed${seed}_train${train_size}_${quality} \
+    -save_trained_model_dir ${save_model_path_prefix}${pretrained_model}_hans_seed${seed}_train${train_size}_${quality}/ \
+    -eval_method $eval_method \
+    -train_epochs $train_epochs 
+
+fi
+
+
