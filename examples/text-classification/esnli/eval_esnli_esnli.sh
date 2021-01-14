788844ad70a746880fd1f2c891fd5781d401f032
rosafish <zhour@carleton.edu>
2021-01-14 14:51:05 -0800

    add sh args and sh file for different models for eval

---
+++ examples/text-classification/esnli/eval_esnli_esnli.sh
@@ -0,0 +1,32 @@
+seqclas_pretrained_model=esnli #bert or esnli
+bert2bert_pretrained_model=esnli #bert or esnli
+
+# inputs
+quality=$1
+seed=$2
+test_type=$3 # matched or mismatched
+training_size=$4
+
+server=uchi # ego or uchi
+
+if [ $server = ego ]; then
+
+    bert2bert_gen_data_path_prefix=../../EncoderDecoderModel/esnli/save_best_models/
+    model_path_prefix=./save_best_model/
+
+elif [ $server = uchi ]; then
+
+    bert2bert_gen_data_path_prefix=/net/scratch/zhouy1/randomness_experiment/edm/
+    model_path_prefix=/net/scratch/zhouy1/randomness_experiment/seqclas/
+
+fi
+
+python ../run_glue.py \
+	--model_name_or_path ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_train${training_size}_${quality}_datafrom${bert2bert_pretrained_model}/best_model/ \
+	--task_name ESNLI \
+	--do_eval \
+	--dev_data_path ${bert2bert_gen_data_path_prefix}${bert2bert_pretrained_model}_hans_seed${seed}_train${training_size}_${quality}/${test_type}_test_text_esnli_format.csv \
+	--max_seq_length 128 \
+	--output_dir ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_train${training_size}_${quality}_datafrom${bert2bert_pretrained_model}/eval_${test_type}_test/ \
+	--overwrite_cache \
+	--esnli_input_type p+h:a,expl1:b 
