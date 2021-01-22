433a502c18590b8fdf10a1b994ae44a1d3e814b5
rosafish <zhour@carleton.edu>
2021-01-21 19:55:18 -0800

    re-run p+h esnli seed01234 train240

--- examples/text-classification/esnli/jobs/seed01234/ph_seed01234_240.sh
+++ examples/text-classification/esnli/jobs/seed01234/ph_seed01234_240.sh
@@ -3,14 +3,14 @@ cd ..
 train_size=240
 dev_size=48
 
-for seed in `seq 2 4`
+for seed in `seq 0 4`
 do
-	sh run_bert_ph.sh $seed $train_size $dev_size
+	#sh run_bert_ph.sh $seed $train_size $dev_size
 	sh run_esnli_ph.sh $seed $train_size $dev_size
 
 	for test_type in mvmt misvmt mvmist misvmist
 	do
-		sh eval_bert_ph.sh $seed $train_size $test_type
+		#sh eval_bert_ph.sh $seed $train_size $test_type
 		sh eval_esnli_ph.sh $seed $train_size $test_type
 
 	done
