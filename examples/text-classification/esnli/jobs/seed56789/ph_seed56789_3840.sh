7c9064367ed4aa9d0ed2e234da0f37275f05f606
rosafish <zhour@carleton.edu>
2021-01-23 15:59:27 -0800

    ph_seed56789_3840.sh

---
+++ examples/text-classification/esnli/jobs/seed56789/ph_seed56789_3840.sh
@@ -0,0 +1,18 @@
+cd ../..
+
+train_size=3840
+dev_size=768
+
+for seed in `seq 5 9`
+do
+	sh run_bert_ph.sh $seed $train_size $dev_size
+	sh run_esnli_ph.sh $seed $train_size $dev_size
+
+	for test_type in mvmt misvmt mvmist misvmist
+	do
+		sh eval_bert_ph.sh $seed $train_size $test_type
+		sh eval_esnli_ph.sh $seed $train_size $test_type
+
+	done
+done
+
