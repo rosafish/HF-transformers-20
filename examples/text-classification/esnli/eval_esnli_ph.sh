4bfddd4a6ed84d0bef1b8e05e55f322fd18b7bc1
rosafish <zhour@carleton.edu>
2021-04-10 13:08:46 -0700

    update esnli ph paths

--- examples/text-classification/esnli/eval_esnli_ph.sh
+++ examples/text-classification/esnli/eval_esnli_ph.sh
@@ -2,44 +2,35 @@ seqclas_pretrained_model=esnli #bert or esnli
 
 # inputs
 seed=$1
-training_size=$2
-test_type=$3 # mvmt or misvmt or mvmist or misvmist
+partition=$2
+training_size=$3
+test_type=$4 # ivit, ivot, ovit, ovot
 
 quality=empty_expl # fixed for p+h benchmark
 server=uchi # ego or uchi
 
+test_size=300
+
 if [ $server = ego ]; then
 
-	data_path_prefix=/data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/
-    bert2bert_gen_data_path_prefix=../../EncoderDecoderModel/esnli/save_best_models/
+	data_path_prefix=/data/rosa/hans-forked/auto/generated_data/
     model_path_prefix=./save_best_model/
 
 elif [ $server = uchi ]; then
 
-    data_path_prefix=/home/zhouy1/data/randomness_experiment/
-    bert2bert_gen_data_path_prefix=/net/scratch/zhouy1/randomness_experiment/edm/
+    data_path_prefix=/net/scratch/zhouy1/data/generated_data/
     model_path_prefix=/net/scratch/zhouy1/randomness_experiment/label_only/
 
 fi
 
-cp ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_train${training_size}_${quality}/vocab.txt ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_train${training_size}_${quality}/best_model/
-
-if [ $test_type = mvmt ] || [ $test_type = misvmt ]; then
-
-    test_size=12000
-
-elif [ $test_type = mvmist ] || [ $test_type = misvmist ]; then
-
-    test_size=3000
-
-fi
+cp ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_partition${partition}_train${training_size}_${quality}/vocab.txt ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_partition${partition}_train${training_size}_${quality}/best_model/
 
 python ../run_glue.py \
-	--model_name_or_path ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_train${training_size}_${quality}/best_model/ \
+	--model_name_or_path ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_partition${partition}_train${training_size}_${quality}/best_model/ \
 	--task_name ESNLI \
 	--do_eval \
-	--dev_data_path ${data_path_prefix}seed${seed}/${test_type}_test${test_size}_${quality}.csv \
+	--dev_data_path ${data_path_prefix}seed${seed}/partition${partition}/test_${test_type}_${test_size}_${quality}.csv \
 	--max_seq_length 128 \
-	--output_dir ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_train${training_size}_${quality}/eval_${test_type}_test/ \
+	--output_dir ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_partition${partition}_train${training_size}_${quality}/eval_${test_type}_test/ \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b 
--- examples/text-classification/esnli/run_esnli_ph.sh
+++ examples/text-classification/esnli/run_esnli_ph.sh
@@ -2,25 +2,24 @@ seqclas_pretrained_model=esnli #bert or esnli
 
 #inputs
 seed=$1
-training_size=$2
-dev_size=$3
+partition=$2
+training_size=$3
+dev_size=$4 
 
 quality=empty_expl # fixed for p+h benchmark
 test_type=mvmt # does not affect anything
-test_size=3000
+test_size=300
 
 server=uchi # ego or uchi
 
 if [ $server = ego ]; then
 
-    data_path_prefix=/data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/
-    bert2bert_gen_data_path_prefix=../../EncoderDecoderModel/esnli/save_best_models/
+    data_path_prefix=/data/rosa/hans-forked/auto/generated_data/
     model_path_prefix=./save_best_model/
 
 elif [ $server = uchi ]; then
 
-    data_path_prefix=/home/zhouy1/data/randomness_experiment/
-    bert2bert_gen_data_path_prefix=/net/scratch/zhouy1/randomness_experiment/edm/
+    data_path_prefix=/net/scratch/zhouy1/data/generated_data/
     model_path_prefix=/net/scratch/zhouy1/randomness_experiment/label_only/
 
 fi
@@ -40,13 +39,13 @@ python ../run_glue.py \
 	--task_name ESNLI \
 	--do_train \
 	--do_eval \
-	--train_data_path ${data_path_prefix}seed${seed}/train${training_size}_${quality}.csv \
-	--dev_data_path ${data_path_prefix}seed${seed}/dev${dev_size}_${quality}.csv \
-	--test_data_path ${data_path_prefix}seed${seed}/${test_type}${test_size}_${quality}.csv \
+	--train_data_path ${data_path_prefix}seed${seed}/partition${partition}/train_${training_size}_${quality}.csv \
+	--dev_data_path ${data_path_prefix}seed${seed}/partition${partition}/dev_${dev_size}_${quality}.csv \
+	--test_data_path ${data_path_prefix}seed${seed}/partition${partition}/test_${test_type}_${test_size}_${quality}.csv \
 	--max_seq_length 128 \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
-	--output_dir ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_train${training_size}_${quality}/ \
+	--output_dir ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_partition${partition}_train${training_size}_${quality}/ \
 	--overwrite_output_dir \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b \
