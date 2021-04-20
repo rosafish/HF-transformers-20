fa61d2488f9dad689a89558f232b830b373db519
rosafish <rosazhou1997@gmail.com>
2021-04-20 09:48:56 -0500

    compute bleu by template

---
+++ examples/EncoderDecoderModel/esnli/eval_by_template.sh
@@ -0,0 +1,19 @@
+seed=0
+
+for partition in `seq 0 4`
+do
+	for test_type in ivit ovit ivot ovot
+	do
+		for expl_type in nl pt
+		do
+			for model in bert esnli
+			do
+				for train_size in 1 2 4 8 16 32 64
+				do
+					python eval_by_template.py /net/scratch/zhouy1/data/generated_data/seed${seed}/partition${partition}/test_${test_type}_300.csv /net/scratch/zhouy1/randomness_experiment/edm/${model}_hans_seed${seed}_partition${partition}_train${train_size}_${expl_type}/ $expl_type $test_type
+		
+				done
+			done
+		done
+	done
+done
