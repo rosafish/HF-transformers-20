dd3e5bcb660076338d931ffdf5b79e6d267e5331
rosafish <zhour@carleton.edu>
2021-04-26 20:46:17 -0700

    sanity checks

--- examples/EncoderDecoderModel/esnli/worst_test_templates.py
+++ examples/EncoderDecoderModel/esnli/worst_test_templates.py
@@ -123,9 +123,9 @@ def main():
         # print('closest_template_info: ', closest_template_info)
         print('test template: %d, %s' % (test_id, templates[test_id]))
         print('closest 1 train (dist %f): %d, %s' % (closest_template_info[1], closest_template_info[0], templates[closest_template_info[0]]))
-        closest_template_info = jaccard_dist_list_sorted[:1][1]
+        closest_template_info = jaccard_dist_list_sorted[1:2][0]
         print('closest 2 train (dist %f): %d, %s' % (closest_template_info[1], closest_template_info[0], templates[closest_template_info[0]]))
-        closest_template_info = jaccard_dist_list_sorted[:1][2]
+        closest_template_info = jaccard_dist_list_sorted[2:3][0]
         print('closest 3 train (dist %f): %d, %s' % (closest_template_info[1], closest_template_info[0], templates[closest_template_info[0]]))
         print('')
 
