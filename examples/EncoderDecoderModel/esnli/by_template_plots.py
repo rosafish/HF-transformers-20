3ac7b3f7cddeb1158ff12f53c98ffee824fcd446
rosafish <zhour@carleton.edu>
2021-04-27 09:07:26 -0700

    plot heatmap

--- examples/EncoderDecoderModel/esnli/by_template_plots.py
+++ examples/EncoderDecoderModel/esnli/by_template_plots.py
@@ -1,6 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
-from worst_test_templates import sort_tuple
+from worst_test_templates import sort_tuple, get_jaccard_dist, load_templates
+import seaborn as sns
 import sys
 
 def get_bleu_by_templates(bleu_by_temp_path):
@@ -28,6 +29,7 @@ def main():
     test_type = sys.argv[4] 
     model = sys.argv[5]
     train_size = sys.argv[6]
+    input_type = sys.argv[7]
     data_dir_name = 'before_new_setting'
 
     bleu_by_temp_path = '/net/scratch/zhouy1/randomness_experiment/%s/edm/%s_hans_seed%s_partition%s_train%s_%s/%s_test_bleu_by_temp.txt' % \
@@ -37,13 +39,21 @@ def main():
     bleu_list_ascend = get_bleu_by_templates(bleu_by_temp_path) 
 
     # plot histogram
-    bleu_list_histogram = [item[1] for item in bleu_list_ascend]
-    plt.hist(bleu_list_histogram, bins=10)
-    plt.title('Distribution of Templates by BLEU Scores (%s)' % test_type)
-    plt.xlabel('BLEU Scores')
-    plt.ylabel('Number of Templates')
-    plt.savefig('histogram_%s_hans_seed%s_partition%s_train%s_%s_%s_%s.png' % (model, seed, partition, train_size, expl_type, test_type, data_dir_name))
+    # bleu_list_histogram = [item[1] for item in bleu_list_ascend]
+    # plt.hist(bleu_list_histogram, bins=10)
+    # plt.title('Distribution of Templates by BLEU Scores (%s)' % test_type)
+    # plt.xlabel('BLEU Scores')
+    # plt.ylabel('Number of Templates')
+    # plt.savefig('histogram_%s_hans_seed%s_partition%s_train%s_%s_%s_%s.png' % (model, seed, partition, train_size, expl_type, test_type, data_dir_name))
 
+    templates_path = "/home/zhouy1/hans-forked/auto/templates_new.csv"
+    templates = load_templates(templates_path, input_type)
+    jaccard_sim_matrix = np.zeros(118, 118)
+    for i in range(118):
+        for j in range(118):
+            jaccard_sim_matrix[i,j] = get_jaccard_dist(templates, i, j)
+    ax = sns.heatmap(jaccard_sim_matrix, linewidth=0.5)
+    plt.savefig('jaccard_heatmap_%s_hans_seed%s_partition%s_train%s_%s_%s_%s.png' % (model, seed, partition, train_size, expl_type, test_type, data_dir_name))
 
 if __name__=='__main__':
     main()
