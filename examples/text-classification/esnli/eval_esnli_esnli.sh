3750d0fb042a0c4f8f291bb4ae5d97dabf4eb7d6
rosafish <rosazhou1997@gmail.com>
2021-05-24 20:13:19 -0500

    allow validation data to be gold

--- examples/text-classification/esnli/eval_esnli_esnli.sh
+++ examples/text-classification/esnli/eval_esnli_esnli.sh
@@ -8,18 +8,19 @@ partition=$3
 test_type=$4 # mvmt or misvmt or mvmist or misvmist
 training_size=$5
 data_dir_name=$6
+validation=$7 # gold or generated
 
 server=uchi # ego or uchi
 
 if [ $server = ego ]; then
 
     bert2bert_gen_data_path_prefix=../../EncoderDecoderModel/esnli/save_best_models/
-    model_path_prefix=./save_best_model/
+    model_path_prefix=./save_best_model/dev_${validation}/
 
 elif [ $server = uchi ]; then
 
     bert2bert_gen_data_path_prefix=/net/scratch/zhouy1/randomness_experiment/${data_dir_name}/edm/
-    model_path_prefix=/net/scratch/zhouy1/randomness_experiment/${data_dir_name}/seqclas/
+    model_path_prefix=/net/scratch/zhouy1/randomness_experiment/${data_dir_name}/seqclas_dev_${validation}/
 
 	cp ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_partition${partition}_train${training_size}_${quality}_datafrom${bert2bert_pretrained_model}/vocab.txt ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_partition${partition}_train${training_size}_${quality}_datafrom${bert2bert_pretrained_model}/best_model/
 
@@ -35,4 +36,3 @@ python ../run_glue.py \
 	--overwrite_cache \
 	--esnli_input_type p+h:a,expl1:b \
 	--test_data_info ${model_path_prefix}${seqclas_pretrained_model}_hans_seed${seed}_partition${partition}_train${training_size}_${quality}_datafrom${bert2bert_pretrained_model}/eval_${test_type}_test_by_templates.csv
-
--- examples/text-classification/esnli/run_bert_bert.sh
+++ examples/text-classification/esnli/run_bert_bert.sh
@@ -6,7 +6,9 @@ quality=$1
 seed=$2
 partition=$3
 training_size=$4
-data_dir_name=$5
+dev_size=$5
+data_dir_name=$6
+validation=$7 # gold or generated
 
 server=uchi # ego or uchi
 
@@ -14,13 +16,13 @@ if [ $server = ego ]; then
 
     data_path_prefix=/data/rosa/data/hans/in_esnli_format/template_expls/randomness_experiment/
     bert2bert_gen_data_path_prefix=../../EncoderDecoderModel/esnli/save_best_models/
-    model_path_prefix=./save_best_model/
+    model_path_prefix=./save_best_model/dev_${validation}/
 
 elif [ $server = uchi ]; then
 
     data_path_prefix=/net/scratch/zhouy1/data/${data_dir_name}/
     bert2bert_gen_data_path_prefix=/net/scratch/zhouy1/randomness_experiment/${data_dir_name}/edm/
-    model_path_prefix=/net/scratch/zhouy1/randomness_experiment/${data_dir_name}/seqclas/
+    model_path_prefix=/net/scratch/zhouy1/randomness_experiment/${data_dir_name}/seqclas_dev_${validation}/
 
 fi
 
@@ -34,13 +36,23 @@ elif [ $seqclas_pretrained_model = bert ]; then
 
 fi
 
+if [ $validation = gold ]; then
+
+    dev_data_path=${data_path_prefix}seed${seed}/partition${partition}/dev_${dev_size}_${quality}.csv
+
+elif [ $validation = generated ]; then
+
+    dev_data_path=${bert2bert_gen_data_path_prefix}${bert2bert_pretrained_model}_hans_seed${seed}_partition${partition}_train${training_size}_${quality}/dev_text_esnli_format.csv
+
+fi
+
 python ../run_glue.py \
 	--model_name_or_path $model_dir \
 	--task_name HANS \
 	--do_train \
 	--do_eval \
 	--train_data_path ${data_path_prefix}seed${seed}/partition${partition}/train_${training_size}_${quality}.csv \
-	--dev_data_path ${bert2bert_gen_data_path_prefix}${bert2bert_pretrained_model}_hans_seed${seed}_partition${partition}_train${training_size}_${quality}/dev_text_esnli_format.csv \
+	--dev_data_path $dev_data_path \
 	--max_seq_length 128 \
 	--per_device_train_batch_size 32 \
 	--learning_rate 2e-5 \
