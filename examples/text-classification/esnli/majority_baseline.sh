69f5240c5275ec4eefdedf565acb0505075c0dbf
rosafish <rosazhou1997@gmail.com>
2021-07-28 17:55:27 -0500

    update majority baseline sh

--- examples/text-classification/esnli/majority_baseline.sh
+++ examples/text-classification/esnli/majority_baseline.sh
@@ -2,16 +2,13 @@ seed=0
 data_dir_name=$1
 
 for partition in `seq 0 4`
-	do
+do
 	for test_type in ivit ovit ivot ovot
-		do 
-			python majority_baseline.py /net/scratch/zhouy1/data/${data_dir_name}/seed${seed}/partition${partition}/ 1 $test_type
-			python majority_baseline.py /net/scratch/zhouy1/data/${data_dir_name}/seed${seed}/partition${partition}/ 2 $test_type
-			python majority_baseline.py /net/scratch/zhouy1/data/${data_dir_name}/seed${seed}/partition${partition}/ 4 $test_type
-			python majority_baseline.py /net/scratch/zhouy1/data/${data_dir_name}/seed${seed}/partition${partition}/ 8 $test_type
-			python majority_baseline.py /net/scratch/zhouy1/data/${data_dir_name}/seed${seed}/partition${partition}/ 16 $test_type
-			python majority_baseline.py /net/scratch/zhouy1/data/${data_dir_name}/seed${seed}/partition${partition}/ 32 $test_type
-			python majority_baseline.py /net/scratch/zhouy1/data/${data_dir_name}/seed${seed}/partition${partition}/ 64 $test_type
+	do 
+	    for train_size in 1 2 4 8 16
+		do
+			python majority_baseline.py /net/scratch/zhouy1/data/${data_dir_name}/seed${seed}/partition${partition}/ $train_size $test_type
 		done
 	done
+done
 
